911d4b27291727dd26862157ceae0379f100914e
ASATILLOEV BUNYOD (STEVEN) <bunyod1234.ba@gmail.com>
2026-01-28 22:49:49 +0900

    feat: Organize and test pages router system

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  console.log("Document - Page router Rendered");
   return (
     <Html lang="en">
       <Head />
