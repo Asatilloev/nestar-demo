be5d20040df672fb2e2a32ac9c11bd748e91097e
ASATILLOEV BUNYOD (STEVEN) <bunyod1234.ba@gmail.com>
2026-01-29 00:04:34 +0900

    feat: Integration Header and Footer via Layouts

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,6 +1,9 @@
-const PropertyList = () => {
-  console.log("PROPERTY LIST COMPONENT - PAGES ROUTER");
-  return <div>PROPERTY LIST</div>;
+import withLayoutBasic from "@libs/components/layout/LayoutBasic";
+import { Container } from "@mui/material";
+import type { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+  return <Container>PROPERTY LIST</Container>;
 };
 
-export default PropertyList;
+export default withLayoutBasic(PropertyList);
