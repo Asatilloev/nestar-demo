adb1dadeecdba457f65c893964979b243a1f6b92
ASATILLOEV BUNYOD (STEVEN) <bunyod1234.ba@gmail.com>
2026-02-03 21:21:31 +0900

    feat: Develop apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import {light} from "../scss/MaterialTheme";
 import { useState } from "react";
+import { ApolloProvider } from "@apollo/client";
+import client from "apollo/client";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
@@ -13,11 +15,14 @@ export default function App({ Component, pageProps }: AppProps) {
   //@ts-ignore
   const [theme] = useState(createTheme(light));
 
-  // Socket.io, Redux, Mui ThemeProvider can be added here
+  // Socket.io, Redux, Apollo, Mui ThemeProvider can be added here
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
+
   );
 }
